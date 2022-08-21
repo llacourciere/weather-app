cb15b4c14cef61111e130660f6c5dccbe7ab6191
LaLacou <108777796+users.noreply.github.com>
2022-08-21 16:08:38 -0700

    working on adding current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,40 @@ var currentWeatherEl = document.querySelector("#curweather-container")
 var citySearch = document.querySelector("#city-search")
 const API_Key = 'f59572c0b2843a8abecdaa750c6e1f93'
 
-var searchCityForm = (event) => {
+var searchCityForm = (event, city) => {
     event.preventDefault();
     var cityName = cityInputEl.value.trim();
 
     if (cityName) {
         cityInputEl.value = "";
         var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_Key}`;
-    fetch(apiURL).then(data => data.json()).then(data => {
-        let { lat, lon } = data[0];
+        fetch(apiURL).then(data => data.json()).then(data => {
+            let { lat, lon } = data[0];
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_Key}`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_Key}`;
 
-        fetch(url).then(data => data.json()).then(console.log)
-    });
+            fetch(url).then(data => data.json()).then(data => {
+                console.log(data);
+                var weather = data[0]
+                var cWeatherEl = document.createElement('h3')
+                cWeatherEl.textContent = weather.name;
+
+                var weatherEl = document.createElement('p');
+                weatherEl.textContent = weather.weather;
+
+                currentWeatherEl.appendChild(weather);
+
+                currentWeatherEl.appendChild(cWeatherEl);
+            })
+        });
     } else {
         alert("Please enter a City Name")
     }
-    console.log(event);
 };
 
-var displayCurrentWeather = function () { 
-   var city = name[i];
-   var cWeatherEl = document.createElement('a')
-    cWeatherEl.classList = 'list-item flex-row align-center'
+var displayCurrentWeather = function (data) {
 
-    var titleEl = document.createElement('span');
-    titleEl.textContent = city;
-
-    cWeatherEl.appendChild(titleEl);
-
- }
+}
 
 
 
